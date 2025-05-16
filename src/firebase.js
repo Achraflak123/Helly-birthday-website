533f0bf43f69ece2acb56b10b4c8791f6c62ac62
achraf189 <achraflak79@gmail.com>
2025-05-16 22:21:05 +0800

    🔥 Fixed Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,10 +5,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyBR5q3irxUMjYybg4VdQ09Z7WNrTc-ZTuk",
   authDomain: "helly-birthday-website.firebaseapp.com",
   projectId: "helly-birthday-website",
-  storageBucket: "helly-birthday-website.firebasestorage.app",
+  storageBucket: "helly-birthday-website.appspot.com", // FIXED
   messagingSenderId: "179445354136",
   appId: "1:179445354136:web:1302d7ae968f39fc804aed"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
